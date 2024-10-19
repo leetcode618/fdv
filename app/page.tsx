9a8930d7ccd90c4e232d66a9378f04fbe8353ac7
nn-kyduyen-dev <nn.kyduyen.dev@gmail.com>
2024-10-19 14:34:15 +0700

    add: about us section

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import Navbar from "./components/navbar/NavBar";
 
 import { useEffect, useState } from "react";
 import fullPageBgr from "./images/background.png";
+import aboutUsMonitorScreen from "./images/about-us.png";
 
 import styles from "./homepage.module.scss";
+import classNames from "classnames";
 
 const MOBILE_BREAKPOINT = 540;
 
@@ -37,6 +39,7 @@ export default function Home() {
             width: "100%",
           }}
         />
+
         <section className={styles.heroSection}>
           <h1 className={`textWhiteBorder ${styles.pageTitle}`}>Lorem ipsum</h1>
           <h3 className={`textWhiteBorder ${styles.pageSubTitle}`}>
@@ -51,6 +54,27 @@ export default function Home() {
           <button className={styles.learnMoreBtn}>
             <p>learn more</p>
           </button>
+        </section>
+
+        <section className={styles.aboutUsSection}>
+          <div className="relative">
+            <Image
+              src={aboutUsMonitorScreen}
+              alt="about-us-monitor"
+              priority
+              quality={100}
+              className={styles.aboutUsScreen}
+            />
+            <div className="absolute top-0 left-0 p-1 xxs:p-2 sm:p-5 text-[7px]">
+              <p className={`textBlackBorder ${styles.aboutUsTitle}`}>about us</p>
+              <p className={classNames(styles.pageDescription, styles.aboutUsDescription)}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                id tristique eros. Nunc a augue vel ipsum porttitor pellentesque
+                a eget sapien. Ut eu turpis feugiat, porttitor magna tincidunt,
+                viverra metus.
+              </p>
+            </div>
+          </div>
         </section>
       </div>
     </>
