8f196c527990b5e6ee55c74f3748db3a28d2b16c
nn-kyduyen-dev <nn.kyduyen.dev@gmail.com>
2024-10-19 17:00:27 +0700

    update: about us section done

---
+++ app/components/AboutUsSection/AboutUsSection.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Image from "next/image";
+
+import aboutUsBubble from "../../images/about-us-bubble.png";
+import aboutUsMonitorScreen from "../../images/about-us.png";
+import buyNowMeme from "../../images/buy-now-meme.png";
+
+import classNames from "classnames";
+import styles from "./AboutUsSection.module.scss";
+
+export default function AboutUsSection() {
+  return (
+    <section>
+      <div className={styles.aboutUsSpeech}>
+        <div className="relative">
+          <Image
+            src={aboutUsBubble}
+            alt="about-us-bubble"
+            priority
+            quality={100}
+            className={styles.aboutUsBubble}
+          />
+
+          <p
+            className={`pageDescription ${classNames(
+              styles.aboutUsDescription,
+              styles.textInBubbleWrapper
+            )}`}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id
+            tristique eros. Nunc a augue vel ipsum porttitor pellentesque a eget
+            sapien. Ut eu turpis feugiat, porttitor magna tincidunt, viverra
+            metus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id
+            tristique eros. Nunc a augue vel ipsum porttitor pellentesque a eget
+            sapien. Ut eu turpis feugiat, porttitor magna tincidunt, viverra
+            metus.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.aboutUsMonitorScreenSection}>
+        <div className="relative">
+          <Image
+            src={aboutUsMonitorScreen}
+            alt="about-us-monitor"
+            priority
+            quality={100}
+            className={styles.aboutUsScreen}
+          />
+          <div className="absolute top-0 left-0 p-1 xxs:p-2 sm:p-5 text-[7px]">
+            <p className={`textBlackBorder ${styles.aboutUsTitle}`}>about us</p>
+            <p className={`pageDescription ${styles.aboutUsDescription}`}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              id tristique eros. Nunc a augue vel ipsum porttitor pellentesque a
+              eget sapien. Ut eu turpis feugiat, porttitor magna tincidunt,
+              viverra metus.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Image
+        src={buyNowMeme}
+        alt="buy-now-meme"
+        priority
+        quality={100}
+        className={styles.buyNowMeme}
+      />
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import Navbar from "./components/navbar/NavBar";
 
 import { useEffect, useState } from "react";
 import fullPageBgr from "./images/background.png";
-import aboutUsMonitorScreen from "./images/about-us.png";
 
+import AboutUsSection from "./components/AboutUsSection/AboutUsSection";
 import styles from "./homepage.module.scss";
-import classNames from "classnames";
 
 const MOBILE_BREAKPOINT = 540;
 
@@ -45,7 +44,7 @@ export default function Home() {
           <h3 className={`textWhiteBorder ${styles.pageSubTitle}`}>
             dolor sit amet
           </h3>
-          <p className={styles.pageDescription}>
+          <p className="pageDescription">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id
             tristique eros. Nunc a augue vel ipsum porttitor pellentesque a eget
             sapien. Ut eu turpis feugiat, porttitor magna tincidunt, viverra
@@ -56,26 +55,7 @@ export default function Home() {
           </button>
         </section>
 
-        <section className={styles.aboutUsSection}>
-          <div className="relative">
-            <Image
-              src={aboutUsMonitorScreen}
-              alt="about-us-monitor"
-              priority
-              quality={100}
-              className={styles.aboutUsScreen}
-            />
-            <div className="absolute top-0 left-0 p-1 xxs:p-2 sm:p-5 text-[7px]">
-              <p className={`textBlackBorder ${styles.aboutUsTitle}`}>about us</p>
-              <p className={classNames(styles.pageDescription, styles.aboutUsDescription)}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                id tristique eros. Nunc a augue vel ipsum porttitor pellentesque
-                a eget sapien. Ut eu turpis feugiat, porttitor magna tincidunt,
-                viverra metus.
-              </p>
-            </div>
-          </div>
-        </section>
+        <AboutUsSection />
       </div>
     </>
   );
